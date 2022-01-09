a1b447ef6d6e089a54c61e6d7f7b2bfeae88cd5a
Samuel Ogu <hello@samuelogu.com>
2022-01-09 21:46:20 +0100

    methods

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,15 +2,20 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 class Counter extends Component {
+
+    state = {
+        count: 10
+    }
+
     render() {
         return (
-            <div>
-                <h1>Hello world!</h1>
+            <React.Fragment>
+                <span>{this.formatCount()}</span>
                 <div className="mt-2 sm:mt-2 sm:flex sm:justify-start lg:justify-start">
                     <div className="rounded-md shadow">
                         <a href="#"
                            className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
-                            Get started
+                            Increment
                         </a>
                     </div>
                     <div className="mt-3 sm:mt-0 sm:ml-3">
@@ -20,9 +25,15 @@ class Counter extends Component {
                         </a>
                     </div>
                 </div>
-            </div>
+            </React.Fragment>
         );
     }
+
+    formatCount() {
+        const {count} = this.state;
+        return count === 0 ? 'Zero' : count;
+    }
+
 }
 
 export default Counter;

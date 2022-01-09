483252c78a895b52ddceba833c35863ac0e42931
Samuel Ogu <hello@samuelogu.com>
2022-01-09 21:40:11 +0100

    buttons

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -5,7 +5,21 @@ class Counter extends Component {
     render() {
         return (
             <div>
-                Hello world!
+                <h1>Hello world!</h1>
+                <div className="mt-2 sm:mt-2 sm:flex sm:justify-start lg:justify-start">
+                    <div className="rounded-md shadow">
+                        <a href="#"
+                           className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+                            Get started
+                        </a>
+                    </div>
+                    <div className="mt-3 sm:mt-0 sm:ml-3">
+                        <a href="#"
+                           className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
+                            Live demo
+                        </a>
+                    </div>
+                </div>
             </div>
         );
     }

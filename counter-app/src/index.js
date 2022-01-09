27aeffc8c56120b0fbca85aba9abdafdfe349910
Samuel Ogu <hello@samuelogu.com>
2022-01-09 21:30:35 +0100

    import counter component

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Counter from "./components/counter";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>,
   document.getElementById('root')
 );
